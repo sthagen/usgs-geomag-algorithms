71a5e0f253f51d6d7fdbc8611c694579318d824b
pcain-usgs <pcain@usgs.gov>
2021-01-06 16:02:50 -0700

    Test with synthetic notebook example

--- test/adjusted_test/adjusted_test.py
+++ test/adjusted_test/adjusted_test.py
@@ -1,8 +1,161 @@
+import json
+import numpy as np
 from numpy.testing import assert_equal, assert_array_almost_equal
 from obspy.core import UTCDateTime
 import pytest
 
 from geomagio.adjusted.SpreadsheetSummaryFactory import SpreadsheetSummaryFactory
+from geomagio.adjusted.AffineType import AffineType
+
+H_ORD = np.array(
+    [
+        1.79289322,
+        1.83983806,
+        1.91024109,
+        2.51489726,
+        1.78952971,
+        2.81157403,
+        2.36417497,
+        2.43864844,
+        3.25364394,
+        2.27479228,
+        3.47800226,
+        2.95694665,
+        3.03482926,
+        3.84541634,
+        2.92632734,
+        4.01635411,
+        3.59456862,
+        3.69312428,
+        4.27043672,
+        3.82250402,
+    ]
+)
+
+E_ORD = np.array(
+    [
+        4.46592583,
+        3.99414122,
+        4.86042918,
+        4.08398446,
+        4.06789278,
+        4.89701546,
+        3.29221074,
+        4.91313376,
+        3.76430406,
+        3.77979418,
+        4.76896396,
+        2.98259167,
+        4.69899703,
+        3.54330923,
+        3.56461527,
+        4.43474537,
+        2.93993354,
+        4.24945532,
+        3.43005774,
+        3.46078181,
+    ]
+)
+
+Z_ORD = np.array(
+    [
+        2.24118095,
+        2.17339265,
+        2.88988358,
+        1.74783897,
+        3.18842428,
+        2.36825251,
+        2.39116112,
+        3.56453372,
+        1.85923781,
+        3.7224498,
+        2.71235632,
+        2.74095046,
+        3.96752743,
+        2.26503334,
+        4.0367477,
+        3.13361064,
+        3.18331114,
+        4.12716967,
+        3.00224898,
+        4.03024472,
+    ]
+)
+
+X_ABS = np.array(
+    [
+        -1.00000000e00,
+        -8.99979996e-01,
+        -7.99959992e-01,
+        -6.99939988e-01,
+        -5.99919984e-01,
+        -4.99899980e-01,
+        -3.99879976e-01,
+        -2.99859972e-01,
+        -1.99839968e-01,
+        -9.98199640e-02,
+        2.00040008e-04,
+        1.00220044e-01,
+        2.00240048e-01,
+        3.00260052e-01,
+        4.00280056e-01,
+        5.00300060e-01,
+        6.00320064e-01,
+        7.00340068e-01,
+        8.00360072e-01,
+        9.00380076e-01,
+    ]
+)
+
+Y_ABS = np.array(
+    [
+        0.0,
+        -0.35248241,
+        0.18456572,
+        0.22223661,
+        -0.64867776,
+        0.86607699,
+        -0.7390734,
+        0.29000341,
+        0.30840017,
+        -0.80892729,
+        0.99997154,
+        -0.80006479,
+        0.29431378,
+        0.30363751,
+        -0.74702288,
+        0.8657967,
+        -0.64130022,
+        0.211858,
+        0.19298425,
+        -0.35563497,
+    ]
+)
+
+Z_ABS = np.array(
+    [
+        0.0,
+        -0.25649982,
+        0.57096366,
+        -0.67874509,
+        0.46830885,
+        0.0032657,
+        -0.54209456,
+        0.90883553,
+        -0.93002867,
+        0.57937261,
+        0.00754126,
+        -0.59148311,
+        0.93449629,
+        -0.904239,
+        0.53078497,
+        0.00979431,
+        -0.47785966,
+        0.68164505,
+        -0.56760976,
+        0.25067805,
+    ]
+)
 
 
 def get_spreadsheet_readings(path, observatory, starttime, endtime):
@@ -51,3 +204,69 @@ def test_Caldata_summaries():
         assert_equal(reading.pier_correction, -0.5)
     # assert that the number of readings equals the number of file within directory
     assert_equal(len(readings), 10)
+
+
+def get_affine_result(type, weights=1):
+    matrix = type.calculate_matrix(
+        ord_hez=(H_ORD, E_ORD, Z_ORD),
+        abs_xyz=(X_ABS, Y_ABS, Z_ABS),
+        weights=weights,
+    )
+    return matrix
+
+
+def test_Affine_result():
+    # load results
+    with open("etc/adjusted/synthetic_results.json") as file:
+        expected = json.load(file)
+    # numbers in keys pertain to method numbers from original documentation(generate_affine_...)
+    assert_array_almost_equal(
+        get_affine_result(AffineType.NO_CONSTRAINTS),
+        np.array(expected["zero"]),
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        get_affine_result(AffineType.Z_ROTATION),
+        np.array(expected["one"]),
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        get_affine_result(AffineType.Z_ROTATION_HSCALE),
+        np.array(expected["two"]),
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.Z_ROTATION_HSCALE_ZBASELINE)),
+        np.array(expected["three"]),
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.ROTATION_TRANSLATION_3D)),
+        expected["four"],
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.RESCALE_3D)),
+        expected["five"],
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.TRANSLATE_ORIGINS)),
+        expected["six"],
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.SHEAR_YZ)),
+        expected["seven"],
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.ROTATION_TRANSLATION_XY)),
+        expected["eight"],
+        decimal=6,
+    )
+    assert_array_almost_equal(
+        list(get_affine_result(AffineType.QR_FACTORIZATION)),
+        expected["nine"],
+        decimal=6,
+    )
