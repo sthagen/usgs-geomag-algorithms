d789971c6a912d4582a107a27ab41848d0d94d32
pcain-usgs <pcain@usgs.gov>
2021-01-08 15:36:01 -0700

    Generate state dictionary in object definition

--- geomagio/adjusted/Affine.py
+++ geomagio/adjusted/Affine.py
@@ -7,6 +7,20 @@ from .Generator import Generator
 from .GeneratorType import GeneratorType
 
 
+def create_states(matrices, pier_correction):
+    if matrices is None:
+        return []
+    states = []
+    for matrix in matrices:
+        data = {"PC": pier_correction}
+        length = len(matrix[0, :])
+        for i in range(0, length):
+            for j in range(0, length):
+                key = "M" + str(i + 1) + str(j + 1)
+                data[key] = matrix[i, j]
+        states.append(data)
+
+
 class Affine(BaseModel):
     observatory: str = None
     starttime: UTCDateTime = UTCDateTime() - (86400 * 7)
@@ -19,4 +33,5 @@ class Affine(BaseModel):
     ]
     pier_correction: float = None
     # TODO: specify array type with fixed shape
-    matrix: Any = None
+    matrices: Any = None
+    states: List[dict] = create_states(matrices, pier_correction)
