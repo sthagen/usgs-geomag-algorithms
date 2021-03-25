743b7f8322a2ab58ed5f3737ecc871926be7d381
pcain-usgs <pcain@usgs.gov>
2021-03-25 09:55:06 -0600

    i/o metadata for generate_matrix

--- geomagio/metadata/__init__.py
+++ geomagio/metadata/__init__.py
@@ -1,7 +1,13 @@
 from .Metadata import Metadata
 from .MetadataCategory import MetadataCategory
-from .MetadataFactory import MetadataFactory
+from .MetadataFactory import MetadataFactory, GEOMAG_API_URL
 from .MetadataQuery import MetadataQuery
 
 
-__all__ = ["Metadata", "MetadataCategory", "MetadataFactory", "MetadataQuery"]
+__all__ = [
+    "GEOMAG_API_URL",
+    "Metadata",
+    "MetadataCategory",
+    "MetadataFactory",
+    "MetadataQuery",
+]
--- geomagio/processing/adjusted.py
+++ geomagio/processing/adjusted.py
@@ -4,7 +4,14 @@ from typing import Optional
 import typer
 
 from ..adjusted.Affine import Affine
-from ..residual import SpreadsheetSummaryFactory, WebAbsolutesFactory
+from ..residual import Reading, SpreadsheetSummaryFactory, WebAbsolutesFactory
+from ..metadata import (
+    GEOMAG_API_URL,
+    Metadata,
+    MetadataCategory,
+    MetadataFactory,
+    MetadataQuery,
+)
 
 
 def main():
@@ -17,8 +24,10 @@ def generate_matrix(
     endtime: str,
     readings_starttime: str,
     readings_endtime: str,
-    output_file: str,
     input_factory: str = "webabsolutes",
+    metadata_url: str = GEOMAG_API_URL,
+    output_file: Optional[str] = None,
+    output_metadata: bool = False,
     spreadsheet_directory: Optional[str] = None,
     webabsolutes_url: Optional[
         str
@@ -38,6 +47,16 @@ def generate_matrix(
             starttime=UTCDateTime(readings_starttime),
             endtime=UTCDateTime(readings_endtime),
         )
+    elif input_factory == "metadata":
+        metadata = MetadataFactory(url=metadata_url).get_metadata(
+            query=MetadataQuery(
+                station=observatory,
+                starttime=readings_starttime,
+                endtime=readings_endtime,
+                category=MetadataCategory.READING,
+            )
+        )
+        readings = [Reading(**m.metadata) for m in metadata]
     else:
         readings = []
 
@@ -48,5 +67,22 @@ def generate_matrix(
     ).calculate(readings=readings)[0]
     result.matrix = result.matrix.tolist()
 
-    with open(output_file, "w") as file:
-        json.dump(result.dict(), file)
+    if output_metadata:
+        MetadataFactory(url=metadata_url).create_metadata(
+            metadata=Metadata(
+                station=observatory,
+                created_by="generate_matrix",
+                metadata=result.dict(),
+                starttime=starttime,
+                endtime=endtime,
+                network="NT",
+                category=MetadataCategory.ADJUSTED_MATRIX,
+            )
+        )
+
+    if output_file:
+        with open(output_file, "w") as file:
+            json.dump(result.dict(), file)
+
+    if not output_file and not output_metadata:
+        raise ValueError("Output method not provided")
