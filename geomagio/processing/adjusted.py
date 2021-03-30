a686fc91ef612b51e98078f6d16aa66fb623f556
pcain-usgs <pcain@usgs.gov>
2021-03-30 12:56:21 -0600

    Add quiet option, enumerate factories, set update interval to none

--- geomagio/processing/adjusted.py
+++ geomagio/processing/adjusted.py
@@ -1,4 +1,5 @@
-import json
+from enum import Enum
+
 from obspy import UTCDateTime
 from typing import Optional
 import typer
@@ -14,6 +15,12 @@ from ..metadata import (
 )
 
 
+class InputFactory(str, Enum):
+    METADATA = "metadata"
+    SPREADSHEET = "spreadsheet"
+    WEBABSOLUTES = "webabsolutes"
+
+
 def main():
     typer.run(generate_matrix)
 
@@ -24,7 +31,7 @@ def generate_matrix(
     endtime: str,
     readings_starttime: str,
     readings_endtime: str,
-    input_factory: str = "webabsolutes",
+    input_factory: InputFactory = InputFactory.WEBABSOLUTES,
     metadata_url: str = GEOMAG_API_URL,
     output_file: Optional[str] = None,
     output_metadata: bool = False,
@@ -32,22 +39,9 @@ def generate_matrix(
     webabsolutes_url: Optional[
         str
     ] = "https://geomag.usgs.gov/baselines/observation.json.php",
+    quiet: bool = False,
 ):
-    if input_factory == "spreadsheet":
-        readings = SpreadsheetSummaryFactory(
-            base_directory=spreadsheet_directory
-        ).get_readings(
-            observatory=observatory,
-            starttime=UTCDateTime(readings_starttime),
-            endtime=UTCDateTime(readings_endtime),
-        )
-    elif input_factory == "webabsolutes":
-        readings = WebAbsolutesFactory(url=webabsolutes_url).get_readings(
-            observatory=observatory,
-            starttime=UTCDateTime(readings_starttime),
-            endtime=UTCDateTime(readings_endtime),
-        )
-    elif input_factory == "metadata":
+    if input_factory == InputFactory.METADATA:
         metadata = MetadataFactory(url=metadata_url).get_metadata(
             query=MetadataQuery(
                 station=observatory,
@@ -59,13 +53,26 @@ def generate_matrix(
             )
         )
         readings = [Reading(**m.metadata) for m in metadata]
-    else:
-        readings = []
-
+    elif input_factory == InputFactory.SPREADSHEET:
+        readings = SpreadsheetSummaryFactory(
+            base_directory=spreadsheet_directory
+        ).get_readings(
+            observatory=observatory,
+            starttime=UTCDateTime(readings_starttime),
+            endtime=UTCDateTime(readings_endtime),
+        )
+    elif input_factory == InputFactory.WEBABSOLUTES:
+        readings = WebAbsolutesFactory(url=webabsolutes_url).get_readings(
+            observatory=observatory,
+            starttime=UTCDateTime(readings_starttime),
+            endtime=UTCDateTime(readings_endtime),
+        )
+    # calculate one affine matrix between starttime and endtime
     result = Affine(
         observatory=observatory,
         starttime=UTCDateTime(starttime),
         endtime=UTCDateTime(endtime),
+        update_interval=None,
     ).calculate(readings=readings)[0]
 
     if output_metadata:
@@ -86,5 +93,5 @@ def generate_matrix(
         with open(output_file, "w") as file:
             file.write(result.json())
 
-    if not output_file and not output_metadata:
+    if not quiet:
         print(result.json(indent=2))
