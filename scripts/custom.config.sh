37cfb475981988fa67882c2d8e878b87b5ab64c7
pcain-usgs <pcain@usgs.gov>
2021-04-16 13:05:15 -0600

    add deploy secure ws jobs

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -9,6 +9,16 @@ export DATA_HOST=${DATA_HOST:-cwbpub.cr.usgs.gov};
 export DATA_PORT=${DATA_PORT:-2060};
 export DATA_TYPE=${DATA_TYPE:-edge};
 
+# Secure Webservice Environment Variables
+export DATABASE_URL=${DATABASE_URL:-""}
+export OPENID_CLIENT_ID=${OPENID_CLIENT_ID:-""}
+export OPENID_CLIENT_SECRET=${OPENID_CLIENT_SECRET:-""}
+export OPENID_METADATA_URL=${OPENID_METADATA_URL:-""}
+export SECRET_KEY=${SECRET_KEY:-""}
+export SECRET_SALT=${SECRET_SALT:-""}
+export ADMIN_GROUP=${ADMIN_GROUP:-""}
+export REVIEWER_GROUP=${REVIEWER_GROUP:-""}
+
 if [[ $TARGET_HOSTNAME == *"mage"* ]]; then
   export DATA_HOST=${TARGET_HOSTNAME}
 fi
