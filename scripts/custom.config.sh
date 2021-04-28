9bde614583711ae987342479570b4c430b1e82d3
Jeremy Fee <jmfee@usgs.gov>
2021-04-28 13:12:52 -0600

    Add configuration guard for ehp server database

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -2,17 +2,12 @@
 
 function get_ehp_server_ini {
   local key=$1; shift;
-  local default_value=${2:-""}; shift;
 
-  if [ ! -f "${EHP_SERVER_INI}" ]; then
-    echo "${default_value}";
-  else
-    grep "${key}" "${EHP_SERVER_INI}" \
-      | tail -n 1 \
-      | cut -d '=' -f 2 \
-      | tr -d ' ' \
-    ;
-  fi
+  grep "${key}" "${EHP_SERVER_INI}" \
+    | tail -n 1 \
+    | cut -d '=' -f 2 \
+    | tr -d ' ' \
+  ;
 }
 
 export SITE_URL="${SITE_URL_PREFIX}geomag${SITE_URL_SUFFIX}";
@@ -27,12 +22,15 @@ export DATA_TYPE=${DATA_TYPE:-edge};
 
 # Web Service Environment Variables
 
-# read database connection from ehpserver.ini
-DB_HOST=$(get_ehp_server_ini 'restricted_database=');
-DB_NAME=geomag_operations
-DB_USER=$(get_ehp_server_ini 'mysql_web_absolutes_user=');
-DB_PASS=$(get_ehp_server_ini 'mysql_web_absolutes_password=');
-export DATABASE_URL="mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}";
+export DATABASE_URL=${DATABASE_URL:-""}
+if [ -f "${EHP_SERVER_INI}" ]; then
+  # when ehp server ini exists, read connection information
+  DB_HOST=$(get_ehp_server_ini 'restricted_database=');
+  DB_NAME=geomag_operations
+  DB_USER=$(get_ehp_server_ini 'mysql_web_absolutes_user=');
+  DB_PASS=$(get_ehp_server_ini 'mysql_web_absolutes_password=');
+  export DATABASE_URL="mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}";
+fi
 
 export OPENID_CLIENT_ID=${OPENID_CLIENT_ID:-""}
 export OPENID_CLIENT_SECRET=${OPENID_CLIENT_SECRET:-""}
