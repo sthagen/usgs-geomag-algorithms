640a8f2d7f1e1005c72204415e929cef0b3351bf
pcain <pcain@usgs.gov>
2021-09-02 08:57:46 -0600

    add SNCL utilities to MockMiniSeedClient

--- test/edge_test/conftest.py
+++ test/edge_test/conftest.py
@@ -4,6 +4,7 @@ from obspy.clients.neic.client import Client
 import pytest
 
 from geomagio import TimeseriesUtility
+from geomagio.edge import SNCL
 
 
 @pytest.fixture(scope="class")
@@ -20,46 +21,24 @@ def MockMiniSeedClient() -> Client:
             starttime: UTCDateTime,
             endtime: UTCDateTime,
         ):
+            sncl = SNCL(
+                station=station,
+                network=network,
+                channel=channel,
+                location=location,
+            )
             trace = TimeseriesUtility.create_empty_trace(
                 starttime=starttime,
                 endtime=endtime,
                 observatory=station,
                 channel=channel,
-                type=self._get_data_type(location=location),
-                interval=self._get_interval(channel=channel),
+                type=sncl.data_type,
+                interval=sncl.interval,
                 network=network,
                 station=station,
                 location=location,
             )
             trace.data = numpy.ones(trace.stats.npts)
             return Stream([trace])
-
-        def _get_interval(self, channel: str) -> str:
-            channel_start = channel[0]
-            if channel_start == "B":
-                return "tenhertz"
-            elif channel_start == "L":
-                return "second"
-            elif channel_start == "U":
-                return "minute"
-            elif channel_start == "R":
-                return "hour"
-            elif channel_start == "P":
-                return "day"
-            else:
-                raise ValueError(f"Unexpected channel start: {channel_start}")
-
-        def _get_data_type(self, location: str) -> str:
-            location_start = location[0]
-            if location_start == "R":
-                return "variation"
-            elif location_start == "A":
-                return "adjusted"
-            elif location_start == "Q":
-                return "quasi-definitive"
-            elif location_start == "D":
-                return "definitive"
-            else:
-                raise ValueError(f"Unexpected location start: {location_start}")
 
     yield MockMiniSeedClient
